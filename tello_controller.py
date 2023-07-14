1508d556583adc5e9c89e9627c704819ff3c6448
BlooMaz <muhdmuaz121@gmail.com>
2023-07-14 16:13:49 +0800

    drone controller

--- tello_controller.py
+++ tello_controller.py
@@ -1,4 +1,4 @@
-import cv2
+import pygame
 from djitellopy import Tello
 
 # Initialize the Tello drone
@@ -7,45 +7,52 @@ drone = Tello()
 # Connect to the Tello drone
 drone.connect()
 
-# Set up video capture
-cap = cv2.VideoCapture(0)
+# Initialize Pygame
+pygame.init()
 
-# Create a window to display the drone's video stream
-cv2.namedWindow("Tello Stream")
+# Set up the Pygame window
+window_width, window_height = 400, 400
+window = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption("Tello Drone Controller")
 
 # Function to send commands to the Tello drone
 def send_command(command):
     drone.send_command(command)
 
 # Main loop
-while True:
-    # Read the video stream from the drone
-    ret, frame = cap.read()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-    # Display the video stream
-    cv2.imshow("Tello Stream", frame)
+        # Keyboard events
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
 
-    # Wait for a key press
-    key = cv2.waitKey(1) & 0xFF
+            # Example commands
+            if event.key == pygame.K_UP:
+                send_command("up 20")
+            elif event.key == pygame.K_DOWN:
+                send_command("down 20")
+            elif event.key == pygame.K_LEFT:
+                send_command("left 20")
+            elif event.key == pygame.K_RIGHT:
+                send_command("right 20")
+            elif event.key == pygame.K_SPACE:
+                send_command("takeoff")
+            elif event.key == pygame.K_RETURN:
+                send_command("land")
 
-    # Exit the loop and close the window if the 'q' key is pressed
-    if key == ord('q'):
-        break
+    # Clear the window
+    window.fill((0, 0, 0))
 
-    # Example commands
-    if key == ord('t'):
-        drone.takeoff()
-    elif key == ord('l'):
-        drone.land()t
-    elif key == ord('w'):
-        send_command("forward 10")
-    elif key == ord('s'):
-        send_command("back 10")
-    elif key == ord('a'):
-        send_command("left 10")
-    elif key == ord('d'):
-        send_command("right 10")
+    # Update the window
+    pygame.display.flip()
 
-# Release the video capture and close the window
-cap.release()
-cv2.destroyAllWindows()
+# Disconnect from the Tello drone
+drone.disconnect()
+
+# Quit Pygame
+pygame.quit()
