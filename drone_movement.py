e5873155de55205332e2806f276775849b87fd2c
BlooMaz <muhdmuaz121@gmail.com>
2023-07-15 16:54:47 +0800

    drone controller

--- drone_movement.py
+++ drone_movement.py
@@ -7,15 +7,23 @@ tello.connect()
 tello.takeoff()
 
 # Define a list of coordinates
-coordinates = [(100, 100), (200, 100), (200, 200), (100, 200), (100, 100)]
-
+coordinates = [[100, 100], [200, 100], [100, 300], [100, 200], [100, 100]]
+horizontal = 0
+vertical = 0
 # Iterate over the list of coordinates
-for coord in coordinates:
-    x, y = coord
+for i in range(len(coordinates)):
+    x = coordinates[i][0]
+    y  = coordinates[i][1]
+
+    horizontal = x - horizontal
+    vertical = y - vertical
+    print(horizontal)
+    print(vertical)
+    print(" ")
     tello.send_rc_control(x, y, 0, 0)
     time.sleep(2)  # Adjust the duration as needed for each movement
 
-# Stop the drone
+
 tello.send_rc_control(0, 0, 0, 0)
 time.sleep(2)
 
